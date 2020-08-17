7e5981e8877cbb5fc5530269e1ea40d38befdbc8
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-17 18:45:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |element|
-    puts if element(name_hash) < 1 
+  name_hash.each do |:key, :value|
+    puts :key if  element(name_hash) < 1 
     
 
 end
